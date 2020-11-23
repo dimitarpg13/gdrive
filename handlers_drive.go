6c5398624c4653fc5b8d081da742cf45d24bac52
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2020-11-23 09:12:52 -0500

    adding valid gdrive client_id

--- handlers_drive.go
+++ handlers_drive.go
@@ -14,8 +14,8 @@ import (
 	"github.com/prasmussen/gdrive/drive"
 )
 
-const ClientId = "367116221053-7n0vf5akeru7on6o2fjinrecpdoe99eg.apps.googleusercontent.com"
-const ClientSecret = "1qsNodXNaWq1mQuBjUjmvhoO"
+const ClientId = "629245460594-qelk0mohlig3mm0ir2sv8pqadkldqe37.apps.googleusercontent.com"
+const ClientSecret = "fHPyQEQDvPcZGZ0SBbNsroNs"
 const TokenFilename = "token_v2.json"
 const DefaultCacheFileName = "file_cache.json"
 
